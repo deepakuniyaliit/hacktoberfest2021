3e919556e2618bd697a81c6c46f5634aee49b3fb
Aakash20022001 <akicollakash@gmail.com>
2021-10-02 12:22:39 +0530

    Add 11_Aakash_Sikarwar to C++

---
+++ C++/11_Aakash_Sikarwar.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+int main()
+{
+    int i,j,row;
+    cout<<"Enter the value for row :";
+    cin>>row;
+    cout << "The number floyd's triangle patterns" << endl;
+    for(i=1; i<=row; i++ ){
+        for(j=1; j<=i; j++ ){
+        	if(j%2==0)
+        	cout<<"0";
+        else
+			
+        cout<<"1";
+    }
+    cout<< endl;
+    }
+    getch();
+    return 0;
+}
