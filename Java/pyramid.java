2d6d603cb444aa426a4f6a120aa3c5ff7e35aac5
Prince Kumar Raj <66669293+Princekumaraj@users.noreply.github.com>
2021-10-04 00:35:33 +0530

    Create pyramid.java
    printing right pyramid in java

---
+++ Java/pyramid.java
@@ -0,0 +1,12 @@
+import java.lang.System;
+class pro{
+    public static void main(String args[]){
+        int r= 5;
+        for(int i=1; i<=r; i++){
+            for(int st=1; st<=i; st++){
+                System.out.print(" * ");
+            }
+            System.out.println();
+        }
+    }
+}
