f4830796b8454735fb003fae9d0b814bac326547
Parth Maradia <71319376+Parth0248@users.noreply.github.com>
2021-10-03 16:14:02 +0530

    Small Website

---
+++ HTML CSS JavaScript/script.js
@@ -0,0 +1,34 @@
+var img = document.getElementById("cent-img");
+
+function ig1() {
+    img.src = "images/crocs doggo.png"
+}
+function ig2() {
+    img.src = "images/ggg.png"
+}
+function ig3() {
+    img.src = "images/lazyy.png"
+}
+function ig4() {
+    img.src = "images/samoyed-2765557_1920.jpg"
+}
+function ig5() {
+    img.src = "images/Screenshot 2021-03-20 110039.png"
+}
+
+
+// for bg color
+
+
+function bg1() {
+    document.body.style.backgroundColor = "#d72631";
+}
+function bg2() {
+    document.body.style.backgroundColor = "#a2d5c6";
+}
+function bg3() {
+    document.body.style.backgroundColor = "#077b8a";
+}
+function bg4() {
+    document.body.style.backgroundColor = "white";
+}
